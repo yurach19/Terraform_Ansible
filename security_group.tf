62d8546458446a7a48732395755f5a0fe3ae607b
yurach19 <132063279+yurach19@users.noreply.github.com>
2023-04-28 14:12:28 +0300

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,17 @@
+# EC2 Security Group
+resource "aws_security_group" "Ansible_security" {
+  name = "Security Ansible"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
